19f68912d46a10fe0e97ab77886732e9a0f06e8f
ketcha2296 <ketcha2296@gmail.com>
2018-10-05 20:49:41 -0600

    Fetch current bill
    If the current bill is { } shows a welcome message, if not shows a try again message.

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,192 +1,3 @@
-// import React from 'react';
-// import {
-//   Image,
-//   Platform,
-//   ScrollView,
-//   StyleSheet,
-//   Text,
-//   TouchableOpacity,
-//   View,
-// } from 'react-native';
-// import { WebBrowser } from 'expo';
-
-// import { MonoText } from '../components/StyledText';
-
-// export default class HomeScreen extends React.Component {
-//   static navigationOptions = {
-//     header: null,
-//   };
-
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-//           <View style={styles.welcomeContainer}>
-//             <Image
-//               source={
-//                 __DEV__
-//                   ? require('../assets/images/robot-dev.png')
-//                   : require('../assets/images/robot-prod.png')
-//               }
-//               style={styles.welcomeImage}
-//             />
-//           </View>
-
-//           <View style={styles.getStartedContainer}>
-//             {this._maybeRenderDevelopmentModeWarning()}
-
-//             <Text style={styles.getStartedText}>Get started by opening</Text>
-
-//             <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-//               <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-//             </View>
-
-//             <Text style={styles.getStartedText}>
-//               Change this text and your app will automatically reload.
-//             </Text>
-//           </View>
-
-//           <View style={styles.helpContainer}>
-//             <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-//               <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-//             </TouchableOpacity>
-//           </View>
-//         </ScrollView>
-
-//         <View style={styles.tabBarInfoContainer}>
-//           <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-//           <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-//             <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-//           </View>
-//         </View>
-//       </View>
-//     );
-//   }
-
-//   _maybeRenderDevelopmentModeWarning() {
-//     if (__DEV__) {
-//       const learnMoreButton = (
-//         <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-//           Learn more
-//         </Text>
-//       );
-
-//       return (
-//         <Text style={styles.developmentModeText}>
-//           Development mode is enabled, your app will be slower but you can use useful development
-//           tools. {learnMoreButton}
-//         </Text>
-//       );
-//     } else {
-//       return (
-//         <Text style={styles.developmentModeText}>
-//           You are not in development mode, your app will run at full speed.
-//         </Text>
-//       );
-//     }
-//   }
-
-//   _handleLearnMorePress = () => {
-//     WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-//   };
-
-//   _handleHelpPress = () => {
-//     WebBrowser.openBrowserAsync(
-//       'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-//     );
-//   };
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//   },
-//   developmentModeText: {
-//     marginBottom: 20,
-//     color: 'rgba(0,0,0,0.4)',
-//     fontSize: 14,
-//     lineHeight: 19,
-//     textAlign: 'center',
-//   },
-//   contentContainer: {
-//     paddingTop: 30,
-//   },
-//   welcomeContainer: {
-//     alignItems: 'center',
-//     marginTop: 10,
-//     marginBottom: 20,
-//   },
-//   welcomeImage: {
-//     width: 100,
-//     height: 80,
-//     resizeMode: 'contain',
-//     marginTop: 3,
-//     marginLeft: -10,
-//   },
-//   getStartedContainer: {
-//     alignItems: 'center',
-//     marginHorizontal: 50,
-//   },
-//   homeScreenFilename: {
-//     marginVertical: 7,
-//   },
-//   codeHighlightText: {
-//     color: 'rgba(96,100,109, 0.8)',
-//   },
-//   codeHighlightContainer: {
-//     backgroundColor: 'rgba(0,0,0,0.05)',
-//     borderRadius: 3,
-//     paddingHorizontal: 4,
-//   },
-//   getStartedText: {
-//     fontSize: 17,
-//     color: 'rgba(96,100,109, 1)',
-//     lineHeight: 24,
-//     textAlign: 'center',
-//   },
-//   tabBarInfoContainer: {
-//     position: 'absolute',
-//     bottom: 0,
-//     left: 0,
-//     right: 0,
-//     ...Platform.select({
-//       ios: {
-//         shadowColor: 'black',
-//         shadowOffset: { height: -3 },
-//         shadowOpacity: 0.1,
-//         shadowRadius: 3,
-//       },
-//       android: {
-//         elevation: 20,
-//       },
-//     }),
-//     alignItems: 'center',
-//     backgroundColor: '#fbfbfb',
-//     paddingVertical: 20,
-//   },
-//   tabBarInfoText: {
-//     fontSize: 17,
-//     color: 'rgba(96,100,109, 1)',
-//     textAlign: 'center',
-//   },
-//   navigationFilename: {
-//     marginTop: 5,
-//   },
-//   helpContainer: {
-//     marginTop: 15,
-//     alignItems: 'center',
-//   },
-//   helpLink: {
-//     paddingVertical: 15,
-//   },
-//   helpLinkText: {
-//     fontSize: 14,
-//     color: '#2e78b7',
-//   },
-// });
-
 import React, {Component} from 'react';
 import {
   Alert,
@@ -197,7 +8,6 @@ import {
   View,
   StatusBar,
   StyleSheet,
-  TouchableOpacity,
   Image,
 } from 'react-native';
 import {BarCodeScanner, Permissions} from 'expo';
@@ -208,137 +18,214 @@ export default class HomeScreen extends Component {
   };
   state = {
     hasCameraPermission: null,
-    lastScannedUrl: null,
+    lastTableId: null,
+    isTaken: null,
+    isLoading: null,
+    dataSource: null
   };
 
   componentDidMount() {
-    this._requestCameraPermission();
+    this.requestCameraPermission();
   }
 
-  _requestCameraPermission = async () => {
+  requestCameraPermission = async () => {
     const {status} = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({
       hasCameraPermission: status === 'granted',
     });
   };
 
-  _handleBarCodeRead = result => {
-    if (result.data !== this.state.lastScannedUrl) {
+  handleBarCodeRead = result => {
+    const {lastTableId} = this.state
+    const tableId = result.data.substring(7, result.data.length)
+    if (tableId !== lastTableId) {
       LayoutAnimation.spring();
-      console.log({result});
       this.setState({
-        lastScannedUrl: result.data.substring(7, result.data.length),
+        lastTableId: result.data.substring(7, result.data.length),
+        isLoading: true,
       });
     }
   };
 
   render() {
-    return (
-      <View style={styles.container}>
-        {this.state.hasCameraPermission === null ? (
-          <Text>Requesting for camera permission</Text>
-        ) : this.state.hasCameraPermission === false ? (
-          <Text style={{color: '#fff'}}>Camera permission is not granted</Text>
-        ) : (
-          <BarCodeScanner
-            onBarCodeRead={this._handleBarCodeRead}
-            style={{
-              height: Dimensions.get('window').height,
-              width: Dimensions.get('window').width,
-            }}
-          >
-            <Image
-              source={require('../assets/images/scan-splash.png')}
-              style={styles.cameraImage}
-            />
-          </BarCodeScanner>
-        )}
+    const {navigate} = this.props.navigation
+    if(this.state.isLoading === null){
+      return (
+        <View style={styles.containerPrincipal}>
+          {this.state.hasCameraPermission === null ? (
+            <Text>Requesting for camera permission</Text>
+          ) : this.state.hasCameraPermission === false ? (
+            <Text style={{color: '#fff'}}>Camera permission is not granted</Text>
+          ) : (
+            <BarCodeScanner
+              onBarCodeRead={this.handleBarCodeRead}
+              style={{
+                height: Dimensions.get('window').height,
+                width: Dimensions.get('window').width,
+              }}
+            >
+              <Image
+                source={require('../assets/images/scan-splash.png')}
+                style={styles.cameraImage}
+              />
+            </BarCodeScanner>
+          )}
 
-        {this._maybeRenderUrl()}
+          {this.maybeRenderUrl()}
 
-        <StatusBar hidden />
-      </View>
-    );
+          <StatusBar hidden />
+        </View>
+      );
+    }else{
+      if(this.state.isLoading === true){
+        //Loading
+        return (
+          <View style={styles.container}>
+              <View style={styles.welcomeContainer}>
+                <Image
+                  source={require('../assets/images/loading.gif')}
+                  style={styles.welcomeImage}
+                />
+              </View>
+    
+              <View style={styles.getStartedContainer}>
+                <Text style={styles.getStartedText}>Loading</Text>
+              </View>
+              {this.maybeRenderUrl()}
+
+              <StatusBar hidden />
+          </View>
+        );
+      }else{
+        if(this.state.isTaken === true){
+          //Taken
+          return (
+            <View style={styles.container}>
+                <View style={styles.welcomeContainer}>
+                  <Image
+                    source={require('../assets/images/robot-dev.png')}
+                    style={styles.welcomeImage}
+                  />
+                </View>
+      
+                <View style={styles.getStartedContainer}>
+                  <Text style={styles.getStartedText}>This table is take, plase try another one.</Text>
+                </View>
+                {this.maybeRenderUrl()}
+        
+                <StatusBar hidden />
+            </View>
+          );
+        }else{
+          //Welcome
+          return (
+            <View style={styles.container}>
+                <View style={styles.welcomeContainer}>
+                  <Image
+                    source={require('../assets/images/robot-dev.png')}
+                    style={styles.welcomeImage}
+                  />
+                </View>
+      
+                <View style={styles.getStartedContainer}>
+                  <Text style={styles.getStartedText}>Welcome to: restaurantName</Text>
+                </View>
+              {this.maybeRenderUrl()}
+
+              <StatusBar hidden />
+            </View>
+          );
+        }
+      }
+    }
   }
 
-  _handlePressUrl = () => {
-    Alert.alert(
-      'Open this URL?',
-      this.state.lastScannedUrl,
-      [
-        {
-          text: 'Yes',
-          onPress: () => Linking.openURL(this.state.lastScannedUrl),
-        },
-        {text: 'No', onPress: () => {}},
-      ],
-      {cancellable: false}
-    );
+  handleCode = (tableId) => {
+    console.log({tableId})
+    if(this.state.isTaken === null){
+      return fetch(`http://alfred-waiter.herokuapp.com/api/tables/${tableId}/currentBill`)
+        .then((response) => response.json())
+        .then((responseJson) => {
+          console.log(responseJson)
+          // this.setState({
+          //   dataSource: responseJson,
+          // });
+          
+          if(Object.keys(responseJson).length !== 0){
+            this.setState({
+              isTaken: true,
+              isLoading: false,
+            })
+          }else{
+            this.setState({
+              isTaken: false,
+              isLoading: false,
+            })
+          }
+          console.log('taken '+this.state.isTaken)
+          console.log('loading '+this.state.isLoading)
+            // hacer el otro fetch 
+            // .then(menu => navegar a la otra vista)
+        })
+        .catch((error) =>{
+          console.error(error);
+        });
+    }
   };
 
-  _handlePressCancel = () => {
-    this.setState({lastScannedUrl: null});
+  handlePressCancel = () => {
+    this.setState({lastTableId: null});
   };
 
-  _maybeRenderUrl = () => {
-    if (!this.state.lastScannedUrl) {
+  maybeRenderUrl = () => {
+    if (!this.state.lastTableId) {
       return;
     }
 
-    return (
-      <View style={styles.bottomBar}>
-        <TouchableOpacity style={styles.url} onPress={this._handlePressUrl}>
-          <Text numberOfLines={1} style={styles.urlText}>
-            {this.state.lastScannedUrl}
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.cancelButton}
-          onPress={this._handlePressCancel}
-        >
-          <Text style={styles.cancelButtonText}>Cancel</Text>
-        </TouchableOpacity>
-      </View>
-    );
+    console.log(this.state.lastTableId)
+    this.handleCode(this.state.lastTableId)
   };
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerPrincipal: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#000',
   },
-  bottomBar: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    padding: 15,
-    flexDirection: 'row',
-  },
-  url: {
+  container: {
     flex: 1,
-  },
-  urlText: {
-    color: '#fff',
-    fontSize: 20,
-  },
-  cancelButton: {
-    marginLeft: 10,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  cancelButtonText: {
-    color: 'rgba(255,255,255,0.8)',
-    fontSize: 18,
+    backgroundColor: '#fff',
   },
   cameraImage: {
     position: 'relative',
     flex: 1,
     height: Dimensions.get('window').height,
     width: Dimensions.get('window').width,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
   },
 });
