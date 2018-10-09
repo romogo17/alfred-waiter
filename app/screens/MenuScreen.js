765d950d5dcefaa3cabcdf3500f0a74468262ec1
ketcha2296 <ketcha2296@gmail.com>
2018-10-09 14:38:50 -0600

    Replace the Button for TouchableOpacity

--- app/screens/MenuScreen.js
+++ app/screens/MenuScreen.js
@@ -200,12 +200,13 @@ const UndefinedMenu = ({navFunction}) => {
           Undefined menu. Please Scan a QR code first.
         </Text>
       </View>
-      <Button
+      <TouchableOpacity
+        style={styles.buttonMenu}
         onPress={navFunction}
-        title="Go to Home"
         accessibilityLabel="Go to Home"
-        style={buttonMenu}
-      />
+      >
+        <Text style={styles.textMenuButton}> Go to Home </Text>
+      </TouchableOpacity>
       <StatusBar hidden />
     </View>
   );
@@ -234,8 +235,15 @@ const styles = StyleSheet.create({
     textAlign: 'justify',
   },
   buttonMenu: {
+    alignItems: 'center',
     backgroundColor: '#fff',
-    color: '#86b0f4',
+    marginTop: 20,
+  },
+  textMenuButton: {
+    color: '#007aff',
+    fontSize: 20,
+    lineHeight: 20,
+    fontStyle: 'italic'
   },
   foodGrid: {
     flex: 1,
